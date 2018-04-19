785c6a5db440c7b47f3e193ea61b335bb88fbc2d
tj <tjvaneerten@gmail.com>
2018-04-19 00:43:32 -0400

    updated sprites with new sheet

--- Assets/Scripts/MovingPlatformController.cs
+++ Assets/Scripts/MovingPlatformController.cs
@@ -6,17 +6,20 @@ public class MovingPlatformController : AbstractSwitchable {
     public float speed;
     public GameObject startingPoint;
     public GameObject endingPoint;
+    private Vector3 endingPointPosition;
 
 	// Use this for initialization
 	protected override void Start () {
         transform.position = startingPoint.transform.position;
+        endingPointPosition = endingPoint.transform.position;
+
 	}
 	
 	// Update is called once per frame
 	protected override void Update () {
 	    if (AllSwitchesAreOn())
         {
-            transform.position = Vector3.MoveTowards(transform.position, endingPoint.transform.position, speed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, endingPointPosition, speed * Time.deltaTime);
         } else
         {
             transform.position = Vector3.MoveTowards(transform.position, startingPoint.transform.position, speed * Time.deltaTime);
