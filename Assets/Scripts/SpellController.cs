59d7cb71b1bca41a8c5c0a51c22291db549d293d
TJ <tjvaneerten@gmail.com>
2018-04-30 21:59:29 -0400

    spell interfaces, fixed spell not working after portals

--- Assets/Scripts/SpellController.cs
+++ Assets/Scripts/SpellController.cs
@@ -4,19 +4,12 @@ using UnityEngine;
 
 public class SpellController : MonoBehaviour {
 
-    private string spellName;
-    private LayerMask impactedLayers;
+    public Spell Spell { get; set; }
 
 	// Use this for initialization
 	void Start () {
 
 	}
-
-    public void Initialize(Spell spell)
-    {
-        spellName = spell.spellName;
-        impactedLayers = spell.impactedLayers;
-    }
 
     // Update is called once per frame
     void Update () {
@@ -25,7 +18,40 @@ public class SpellController : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //Debug.Log(spellName + " hit something");
+        MonoBehaviour go = collision.gameObject.GetComponent<MonoBehaviour>();
+        switch (Spell.spellName)
+        {
+            case "Fire":
+                if (go is IBurnable)
+                {
+                    ((IBurnable)go).Burn();
+                }
+                break;
+            case "Ice":
+                if (go is IFreezable)
+                {
+                    ((IFreezable)go).Freeze();
+                }
+                break;
+            case "Gust":
+                if (go is IGustable)
+                {
+                    ((IGustable)go).Gust();
+                }
+                break;
+            case "Clone":
+                if (go is ICloneable)
+                {
+                    ((ICloneable)go).Clone();
+                }
+                break;
+        }
+
+        if (collision.gameObject.layer != LayerMask.NameToLayer("Portal"))
+        {
+            Destroy(gameObject);
+        }
+
     }
 
 }
