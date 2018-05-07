0a4995740060466fde65b3ed5bbe7c006f8ad6ae
TJ <tjvaneerten@gmail.com>
2018-05-06 23:32:59 -0400

    added timed option to stick switch

--- Assets/Scripts/StickSwitchController.cs
+++ Assets/Scripts/StickSwitchController.cs
@@ -3,8 +3,11 @@ using System.Collections;
 
 public class StickSwitchController : AbstractSwitch {
 
+    public bool isTimedSwitch;
+    public float timedSwitchDuration;
     private Animator animator;
     private bool playerInProximity;
+    private float elapsedTime;
 
 	// Use this for initialization
 	protected override void Start () {
@@ -14,11 +17,20 @@ public class StickSwitchController : AbstractSwitch {
 	
 	// Update is called once per frame
 	protected override void Update () {
+        if(isTimedSwitch && IsSwitchedOn)
+        {
+            elapsedTime += Time.deltaTime;
+            if (elapsedTime > timedSwitchDuration)
+            {
+                IsSwitchedOn = false;
+                elapsedTime = 0;
+            }
+        }
         if (playerInProximity && Input.GetButtonDown("Fire1"))
         {
             IsSwitchedOn = !IsSwitchedOn;
-            animator.SetBool("IsSwitchedOn", IsSwitchedOn);
         }
+        animator.SetBool("IsSwitchedOn", IsSwitchedOn);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
