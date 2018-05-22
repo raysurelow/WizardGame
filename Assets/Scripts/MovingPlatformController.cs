57b479d9228a4cefbdbc8f50b8730083bc77b9e4
raysurelow <ray.schierloh@gmail.com>
2018-05-21 23:24:53 -0400

    make moving platform start/end positions child objects, slow down wizard walking animation

--- Assets/Scripts/MovingPlatformController.cs
+++ Assets/Scripts/MovingPlatformController.cs
@@ -4,15 +4,16 @@ using System.Collections;
 public class MovingPlatformController : AbstractSwitchable {
 
     public float speed;
-    public GameObject startingPoint;
-    public GameObject endingPoint;
+    private Vector3 startingPointPosition;
     private Vector3 endingPointPosition;
+
 
 	// Use this for initialization
 	protected override void Start () {
-        transform.position = startingPoint.transform.position;
-        endingPointPosition = endingPoint.transform.position;
-
+        Transform startingPoint = transform.Find("StartingPoint");
+        startingPointPosition = startingPoint.position;
+        Transform endingPoint = transform.Find("EndingPoint");
+        endingPointPosition = endingPoint.position;
 	}
 	
 	// Update is called once per frame
@@ -22,7 +23,7 @@ public class MovingPlatformController : AbstractSwitchable {
             transform.position = Vector3.MoveTowards(transform.position, endingPointPosition, speed * Time.deltaTime);
         } else
         {
-            transform.position = Vector3.MoveTowards(transform.position, startingPoint.transform.position, speed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, startingPointPosition, speed * Time.deltaTime);
         }
 	}
 }
