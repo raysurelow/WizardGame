168ef9c8c1cad43e9633a394a5f6ffcd2e26ad02
Ray <ray.schierloh@gmail.com>
2018-11-13 20:44:03 -0500

    box controller update

--- Assets/Scripts/BoxController.cs
+++ Assets/Scripts/BoxController.cs
@@ -133,9 +133,6 @@ public class BoxController : MonoBehaviour, IFreezable, ICloneable, IBurnable, I
     {
         if (!isFrozen)
         {
-            rigidBody.velocity = Vector2.zero;
-            rigidBody.angularVelocity = 0f;
-            transform.position = startingPosition;
             isCloned = false;
         }
         else
@@ -154,5 +151,12 @@ public class BoxController : MonoBehaviour, IFreezable, ICloneable, IBurnable, I
         {
             rigidBody.AddForce(new Vector2(-10, 0), ForceMode2D.Impulse);
         }
+    }
+
+    public void ResetBox()
+    {
+        rigidBody.velocity = Vector2.zero;
+        rigidBody.angularVelocity = 0f;
+        transform.position = startingPosition;
     }
 }
